1e2666561d51fa92f5bef0f61556750c28334972
mercytv932 <Mercytv932@gmail.com>
2026-04-16 02:09:05 -0400

    Add files via upload
    Adding the "About" section

---
+++ About.js
@@ -0,0 +1,21 @@
+
+const About = () =>{
+  return (
+    <div className="aboutContainer">
+      <h1>About Me</h1>
+      <p>I'm Yosef Ergano, a Software Engineer with 2+ years of expiernce in the field. 
+        I especialize in the frontend-side as well as some expiernce in the backend.
+      </p>
+      <h3>Skills</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+      </ul>
+    </div>
+  )
+}
+
+export default About;
