369dda66c86262d7891cb712528e162966710847
mercytv932 <Mercytv932@gmail.com>
2026-04-16 01:53:01 -0400

    Add files via upload
    Adding Hero Section

---
+++ Hero.js
@@ -0,0 +1,15 @@
+
+const Hero = () => {
+  return(
+    <div className="heroContainer">
+  <h5>Hello, I'm </h5>
+  <h1>Yosef Ergano</h1>
+  <h2>Software Engineer</h2>
+  <p>building web applications using technologies like Html, Css, JavaScript, React, Node.js</p>
+  <button>View My Work</button>
+  <button>Contact Me</button>
+  </div>
+  )
+}
+
+export default Hero;
