658312910f2f45ec4123d1f90cc75e05378bc591
mercytv932 <Mercytv932@gmail.com>
2026-04-15 21:00:42 -0400

    Add files via upload
    Adding Navbar

---
+++ Navbar.js
@@ -0,0 +1,17 @@
+
+const Navbar = () => {
+  return (
+    
+    <nav className="navbar">
+
+      <span>Yosef Ergano</span>
+    <a href="about">About</a>
+    <a href="projects">Projects</a>
+    <a href="contact">Contact</a>
+    <button>Hire Me</button>
+    
+    </nav>
+  )
+}
+
+export default Navbar;
