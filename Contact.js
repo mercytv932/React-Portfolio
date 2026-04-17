62e2bf04a3f435e2ebcbacfb33421940b893019f
mercytv932 <Mercytv932@gmail.com>
2026-04-17 03:25:33 -0400

    Add files via upload
    Adding the Contact section

---
+++ Contact.js
@@ -0,0 +1,16 @@
+
+const Contact = () =>{
+  return (
+    <div className="contactContainer">
+    <h1>Contact</h1>
+    <p><a href="mercytv932@gmail.com">Mercytv932@gmail.com</a></p>
+    <a href="https://github.com/mercytv932">GitHub</a>
+    <br></br>
+    <a href="https://www.linkedin.com/in/yosef-ergano-a694a7266/">Linkedln</a>
+    <p>Feel free to reach out!</p>
+    </div>
+    
+  )
+}
+
+export default Contact;
