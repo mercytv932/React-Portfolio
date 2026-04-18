b708023ca5c392d4c966ab9cd702a587b683d8b7
mercytv932 <Mercytv932@gmail.com>
2026-04-18 01:48:10 -0400

    Wrap buttons in a div in Hero component
    Added a wrapper div around the buttons in the Hero component.

--- Hero.js
+++ Hero.js
@@ -6,8 +6,11 @@ const Hero = () => {
   <h1>Yosef Ergano</h1>
   <h2>Software Engineer</h2>
   <p>building web applications using technologies like Html, Css, JavaScript, React, Node.js</p>
+  <div className="heroBtns">
   <button>View My Work</button>
   <button>Contact Me</button>
+  </div>
+
   </div>
   )
 }
