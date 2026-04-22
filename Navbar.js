e702171f73f4787bea6cb96fa9a3da5c9688732d
mercytv932 <Mercytv932@gmail.com>
2026-04-21 23:54:03 -0400

    Add LinkedIn link to Navbar
    Added a LinkedIn link to the Navbar component.

--- Navbar.js
+++ Navbar.js
@@ -8,7 +8,8 @@ const Navbar = () => {
     <a href="about">About</a>
     <a href="projects">Projects</a>
     <a href="contact">Contact</a>
-    <button>Hire Me</button>
+    <a href="https://www.linkedin.com/in/yosef-ergano-a694a7266/" target="_blank"role="noreferrer">
+    <button>Hire Me</button></a>
     
     </nav>
   )
