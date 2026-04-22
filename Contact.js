90ef672561e4d07f5d62d180f85a61aaca8710b8
mercytv932 <Mercytv932@gmail.com>
2026-04-21 23:53:39 -0400

    Refactor Contact component for email link
    Updated email link to use 'mailto' for direct email access.

--- Contact.js
+++ Contact.js
@@ -3,9 +3,8 @@ const Contact = () =>{
   return (
     <div className="contactContainer">
     <h1>Contact</h1>
-    <p><a href="mercytv932@gmail.com">Mercytv932@gmail.com</a></p>
+    <a href="mailto:Mercytv932@gmail.com">Mercytv932@gmail.com</a>
     <a href="https://github.com/mercytv932">GitHub</a>
-    <br></br>
     <a href="https://www.linkedin.com/in/yosef-ergano-a694a7266/">Linkedln</a>
     <p>Feel free to reach out!</p>
     </div>
