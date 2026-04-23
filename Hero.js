cbf7663f2201221a1372dda3b74ca839dc42d37d
mercytv932 <Mercytv932@gmail.com>
2026-04-22 22:59:51 -0400

    Enhance Hero component with image and structure
    Added a hero image section to the Hero component.

--- Hero.js
+++ Hero.js
@@ -2,6 +2,7 @@
 const Hero = () => {
   return(
     <div className="heroContainer">
+      <div className="heroText">
   <h5>Hello, I'm </h5>
   <h1>Yosef Ergano</h1>
   <h2>Software Engineer</h2>
@@ -9,6 +10,10 @@ const Hero = () => {
   <div className="heroBtns">
   <button>View My Work</button>
   <button>Contact Me</button>
+  </div>
+  </div>
+  <div className="heroImage">
+    <img src="/profile.pic.png" alt="Yosef Ergano"/>
   </div>
 
   </div>
