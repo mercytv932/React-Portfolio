beff5f371f59f43c580ea0dcc0ae103b8a5e0d5a
mercytv932 <Mercytv932@gmail.com>
2026-04-23 23:23:52 -0400

    feat: update site title

--- src/Contact.js
+++ src/Contact.js
@@ -4,12 +4,12 @@ const Contact = () =>{
     <div className="contactContainer" id="contact">
     <h1>Contact</h1>
     <a href="mailto:Mercytv932@gmail.com">Mercytv932@gmail.com</a>
-    <a href="https://github.com/mercytv932">GitHub</a>
-    <a href="https://www.linkedin.com/in/yosef-ergano-a694a7266/">Linkedln</a>
+    <a href="https://github.com/mercytv932" target="_blank"rel="noopener noreferrer">GitHub</a>
+    <a href="https://www.linkedin.com/in/yosef-ergano-a694a7266/" target="_blank"rel="noopener noreferrer">LinkedIn</a>
     <p>Feel free to reach out!</p>
     </div>
     
   )
 }
 
-export default Contact;
+export default Contact
--- src/Hero.js
+++ src/Hero.js
@@ -6,7 +6,7 @@ const Hero = () => {
   <h5>Hello, I'm </h5>
   <h1>Yosef Ergano</h1>
   <h2>Software Engineer</h2>
-  <p>building web applications using technologies like Html, Css, JavaScript, React, Node.js</p>
+  <p>Building full-stack web applications using technologies like HTML, CSS, JavaScript, React, and Node.js</p>
   <div className="heroBtns">
   <button>View My Work</button>
   <button>Contact Me</button>
